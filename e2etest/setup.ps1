4f84d8e0367bb953345e3f35b644ae9a1456e117
hanxiao <hanli@microsoft.com>
2018-10-16 19:06:20 +0800

    test

--- e2etest/setup.ps1
+++ e2etest/setup.ps1
@@ -1,16 +1,17 @@
 $base = pwd;
 
-# Download Functions Core Tools
-Remove-Item -Force "$base\Azure.Functions.Cli.zip" -ErrorAction Ignore
-Remove-Item -Recurse -Force "$base\Azure.Functions.Cli" -ErrorAction Ignore
-Write-Host "Downloading Functions Host...."
-$url = "https://functionsclibuilds.blob.core.windows.net/builds/2/latest/Azure.Functions.Cli.win-x64.zip"
-$output = "$base\Azure.Functions.Cli.zip"
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $output)
-Expand-Archive "$base\Azure.Functions.Cli.zip" -DestinationPath "$base\Azure.Functions.Cli"
+# # Download Functions Core Tools
+# Remove-Item -Force "$base\Azure.Functions.Cli.zip" -ErrorAction Ignore
+# Remove-Item -Recurse -Force "$base\Azure.Functions.Cli" -ErrorAction Ignore
+# Write-Host "Downloading Functions Host...."
+# $url = "https://functionsclibuilds.blob.core.windows.net/builds/2/latest/Azure.Functions.Cli.win-x64.zip"
+# $output = "$base\Azure.Functions.Cli.zip"
+# $wc = New-Object System.Net.WebClient
+# $wc.DownloadFile($url, $output)
+# Expand-Archive "$base\Azure.Functions.Cli.zip" -DestinationPath "$base\Azure.Functions.Cli"
 
 # Clone and install function maven plguin and archetype
+ls
 mvn clean install
 Remove-Item -Recurse -Force "azure-maven-archetypes" -ErrorAction Ignore
 git clone https://github.com/Microsoft/azure-maven-archetypes.git -b develop
@@ -27,11 +28,11 @@ mvn archetype:generate -DarchetypeCatalog="local" -DarchetypeGroupId="com.micros
 mvn -f ".\e2etestproject\pom.xml" clean package
 cd ..
 
-# Run function host
-$Env:AzureWebJobsScriptRoot = "$base\e2etestproject\e2etestproject\target\azure-functions\azure-functions-java-endtoendtests"
-$Env:FUNCTIONS_WORKER_RUNTIME = "java"
-$Env:AZURE_FUNCTIONS_ENVIRONMENT = "development"
-$Env:Path = $Env:Path+";$base\Azure.Functions.Cli"
-$proc = start-process -filepath "$base\Azure.Functions.Cli\func.exe" -WorkingDirectory "$base\e2etestproject\e2etestproject\target\azure-functions\e2etest-java-functions" -ArgumentList "host start" -PassThru
-Start-Sleep -s 30
-return $proc.Id
+# # Run function host
+# $Env:AzureWebJobsScriptRoot = "$base\e2etestproject\e2etestproject\target\azure-functions\azure-functions-java-endtoendtests"
+# $Env:FUNCTIONS_WORKER_RUNTIME = "java"
+# $Env:AZURE_FUNCTIONS_ENVIRONMENT = "development"
+# $Env:Path = $Env:Path+";$base\Azure.Functions.Cli"
+# $proc = start-process -filepath "$base\Azure.Functions.Cli\func.exe" -WorkingDirectory "$base\e2etestproject\e2etestproject\target\azure-functions\e2etest-java-functions" -ArgumentList "host start" -PassThru
+# Start-Sleep -s 30
+# return $proc.Id
